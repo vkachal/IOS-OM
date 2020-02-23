315c92e3addb75bab349379530f2cde29ff19e63
Iryna Koval <irysic.o@gmail.com>
2020-02-23 15:29:13 -0800

    rename test

--- OrderMEUITests/OrderMEUITests.swift
+++ OrderMEUITests/OrderMEUITests.swift
@@ -19,7 +19,7 @@ class OrderMEUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    func testExample() {
+    func testBringMenu() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
