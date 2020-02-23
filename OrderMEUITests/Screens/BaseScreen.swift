58a611855b9b1a67452be30f4253543920dba076
halamelyk <halinamelnyk@gmail.com>
2020-02-22 20:24:13 -0800

    Second commit

---
+++ OrderMEUITests/Screens/BaseScreen.swift
@@ -0,0 +1,24 @@
+//
+//  BaseScreen.swift
+//  OrderMEUITests
+//
+//  Created by Галя Мельник on 2/22/20.
+//  Copyright © 2020 Boris Gurtovoy. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BaseScreen {
+    static let app = XCUIApplication()
+    static let collectionViewsQuery = app.collectionViews
+    var timeout: Double = 5.0
+    
+    func tap(_ element: XCUIElement) {
+        element.tap()
+    }
+    
+    func type(_ text: String, to element: XCUIElement) {
+        element.typeText(text)
+    }
+}
